9fa0354662e1e287f0204ae1a8dbaed4e3d06131
kernelist <xkernelist@gmail.com>
2023-01-02 23:17:36 +0300

    catrs

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use structopt::StructOpt;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+#[derive(StructOpt)]
+#[structopt(name = "catrs")]
+struct Cat {
+    #[structopt(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
+
+fn main() {
+    let args = Cat::from_args();
+    for file in args.files {
+        let mut f = File::open(file).expect("file not found");
+        let mut contents = String::new();
+        f.read_to_string(&mut contents).expect("something went wrong reading the file");
+        println!("{}", contents);
+    }
+}
